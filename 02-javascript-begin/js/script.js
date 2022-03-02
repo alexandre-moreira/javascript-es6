3653efcdb51a4e7cb4af9005b42eaa255aa421b0
Alexandre Moreira <4lexandre.moreira@gmail.com>
2022-03-02 18:02:15 -0300

    Created comment and ternary

---
+++ 02-javascript-begin/js/script.js
@@ -0,0 +1,16 @@
+// Some 500 ao valor de scroll abaixo,
+// atribuindo o novo valor ao scroll
+    var scroll = 1000;
+    scroll += 500;
+    console.log(scroll)
+
+// atribua true para variavel darCredito
+// caso o clinte possua carro e casa
+// e false caso contrario
+    var possuiCarro = true;
+    var possuiCasa = true;
+    var darCredito;
+
+    darCredito = (possuiCarro && possuiCasa);
+    console.log(darCredito)
+
