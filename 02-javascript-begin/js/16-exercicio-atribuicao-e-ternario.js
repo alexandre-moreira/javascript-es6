6e876a49d952ff93833a33f05c68c5505711b334
Alexandre Moreira <4lexandre.moreira@gmail.com>
2022-03-03 13:05:36 -0300

    Create Exercise Comment and Ternary

--- 02-javascript-begin/js/16-exercicio-atribuicao-e-ternario.js
+++ 02-javascript-begin/js/16-exercicio-atribuicao-e-ternario.js
@@ -1,47 +1,16 @@
-// Comentários
-    /*
-        Servem para explicar o código
-    */
+// Some 500 ao valor de scroll abaixo,
+// atribuindo o novo valor ao scroll
+var scroll = 1000;
+scroll += 500;
+console.log(scroll)
 
-    // Comentário de uma linha
+// atribua true para variavel darCredito
+// caso o clinte possua carro e casa
+// e false caso contrario
+var possuiCarro = true;
+var possuiCasa = true;
+var darCredito;
 
-    /*
-        Comentário
-        com diversas
-        linhas
-    */
+darCredito = (possuiCarro && possuiCasa);
+console.log(darCredito)
 
-    // var nome = 'Alexandre'
-
-    /*
-        | Comentar um linha de código
-        | desativa a mesma. Não deixe
-        | linha de código comentadas no arquivo final.
-    */
-
-    // Operadores de Atribuição
-    // Podem funcionar como formas abreviadas
-    var x = 5;
-    var y = 10;
-
-    x += y; // x = x + y (15)
-    x -= y; // x = x - y (-5)
-    x *= y; // x = x * y (50)
-    x /= y; // x = x / y (0.5)
-    x %= y; // x = x % y (0)
-    x **= y; // x = x ** y (9765625)
-
-// Operadore Ternário
-    // Abreviação de condicionais com if e else
-        var idade = 19;
-        var podeBeber = (idade >=18) ? 'Pode Beber' : 'Não pode beber';
-        console.log(podeBeber) // Pode beber
-
-    // Condição ? true : false
-
-    /*
-        | Geralmente utilizado quando
-        | precisamos atribuir um valor
-        | para uma variável, dependendo de
-        | uma condição.
-    */
